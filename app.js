16d021d19a474ecf0c6ad29bb4e98dcb6c08bb6a
Daniel02-03-04 <danielramos550455@gmail.com>
2023-08-08 19:55:41 -0300

    probando render

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const port = process.env.PORT || 3009;
 
 const app = express();
 
@@ -10,4 +11,4 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/home.html'))
 });
 
-app.listen(3009, () => console.log('Servidor corriendo en el puerto 3009'))
+app.listen(port, () => console.log('Servidor corriendo en el puerto 3009'))
